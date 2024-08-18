8024aa030ad19025f6a87d149c181baf89dc50d0
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-08-18 21:24:55 +0900

    scaffdog base ok

---
+++ src/components/shared/Container/SectionContainer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import clsx from "clsx";
+import { FC, ReactNode } from "react";
+
+import { backgroundStyle, container, wrapper } from "./SectionContainer.css";
+
+type Props = {
+    children: ReactNode;
+    title?: string;
+    backgroundColor?: keyof typeof backgroundStyle;
+};
+
+export const SectionContainer: FC<Props> = ({
+    children,
+    title,
+    backgroundColor = "primaryBackground",
+}) => (
+    <div className={wrapper}>
+        <section className={clsx(container, backgroundStyle[backgroundColor])}>
+            {title && <h2>{title}</h2>}
+            {children}
+        </section>
+    </div>
+);
