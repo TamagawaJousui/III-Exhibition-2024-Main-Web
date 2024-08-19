941f488281be41adcee42b772a5c0734ff039236
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-08-20 02:13:08 +0900

    add type definition

--- src/models/global.d.ts
+++ src/models/global.d.ts
@@ -5,3 +5,20 @@ declare type RecursiveLeafKeys<T, Prefix = ""> = {
             : RecursiveLeafKeys<T[K], `${Prefix}${K}.`>
         : `${Prefix}${K}`;
 }[keyof T];
+
+// types/react-water-wave.d.ts
+declare module "react-water-wave" {
+    import * as React from "react";
+
+    interface WaterWaveProps {
+        imageUrl?: string;
+        dropRadius?: number;
+        perturbance?: number;
+        resolution?: number;
+        children?: () => React.ReactNode;
+    }
+
+    const WaterWave: React.FC<WaterWaveProps>;
+
+    export default WaterWave;
+}
