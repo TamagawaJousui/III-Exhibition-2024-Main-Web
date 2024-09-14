bb8c9de9684f666515f209b163dead5496d9ce2e
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-09-14 15:51:53 +0900

    delete teaser section

--- src/page/home/page.tsx
+++ src/page/home/page.tsx
@@ -1,11 +1,9 @@
-import { TeaserSection } from "@/components/section/Teaser/Teaser";
 import { Footer } from "@/components/shared/Footer";
 
 import { styles } from "./page.css";
 
 export const HomePage = () => (
     <div className={styles.root}>
-        <TeaserSection />
         <Footer />
     </div>
 );
