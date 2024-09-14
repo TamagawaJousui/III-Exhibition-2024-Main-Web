25676aa703e882cb89ebc6d4f67027f31235859f
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-09-14 18:27:23 +0900

    add x scroll

--- src/page/home/page.css.ts
+++ src/page/home/page.css.ts
@@ -1,14 +1,22 @@
 import { style } from "@vanilla-extract/css";
 
+import { typography } from "@/styles/typography.css";
+
 import { vars } from "@/styles";
 
 export const styles = {
-    root: style({
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        backgroundColor: vars.color.primaryBackground,
-        height: "100vh",
-        maxHeight: "100vh",
-    }),
+    root: style([
+        typography({}),
+        {
+            display: "flex",
+            background: vars.color.background.default,
+            height: "100vh",
+            overflowX: "scroll",
+            /*スクロールバー非表示（IE・Edge）*/
+            msOverflowStyle: "none",
+            /*スクロールバー非表示（Firefox）*/
+            scrollbarWidth: "none",
+            whiteSpace: "nowrap",
+        },
+    ]),
 };
--- src/page/home/page.tsx
+++ src/page/home/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+import { ConceptSection } from "@/components/section/concept";
+import { WorksSection } from "@/components/section/works";
 import { Footer } from "@/components/shared/Footer";
 
 import { styles } from "./page.css";
 
-export const HomePage = () => (
-    <div className={styles.root}>
-        <Footer />
-    </div>
-);
+export const HomePage = () => {
+    const scrollContainer = useRef<HTMLDivElement>(null);
+
+    /**
+     * NOTE: 横スクロールにするための処理
+     * スマホ版対応の際に変更するかも
+     */
+    useEffect(() => {
+        const container = scrollContainer.current;
+        if (container) {
+            const handleWheel = (event: WheelEvent) => {
+                event.preventDefault();
+                container.scrollLeft += event.deltaY;
+            };
+
+            container.addEventListener("wheel", handleWheel);
+            return () => container.removeEventListener("wheel", handleWheel);
+        }
+    }, []);
+
+    return (
+        <div className={styles.root} ref={scrollContainer}>
+            <ConceptSection />
+            <WorksSection />
+            <Footer />
+        </div>
+    );
+};
