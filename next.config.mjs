5f61f6ead9f1e1e64db52c93358854499232c182
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-09-15 17:46:41 +0900

    setup for placehold

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,17 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
-    reactStrictMode: true,
+  reactStrictMode: true,
+  images: {
+    // FIXME: テスト用の設定追加なので、本番環境では削除する
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default withVanillaExtract(nextConfig);
