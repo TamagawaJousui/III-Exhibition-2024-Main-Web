cd73ec38d234552c8a8f42c40449370a96490770
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-10-03 01:46:20 +0900

    base ok

--- src/utils/hocs/WithTitle/WithTitle.tsx
+++ src/utils/hocs/WithTitle/WithTitle.tsx
@@ -11,10 +11,11 @@ type Props = {
     size: NonNullable<React.ComponentProps<typeof typography>>["fontSize"];
     fit?: boolean;
     children: React.ReactNode;
+    className?: string;
 };
 
-export const WithTitle: FC<Props> = ({ title, size, fit = false, children }) => (
-    <div className={styles.root}>
+export const WithTitle: FC<Props> = ({ title, size, fit = false, children, className }) => (
+    <div className={clsx(styles.root, className)}>
         <h3 className={clsx(typography({ fontSize: size }), styles.heading({ fit: fit }))}>
             {title}
         </h3>
