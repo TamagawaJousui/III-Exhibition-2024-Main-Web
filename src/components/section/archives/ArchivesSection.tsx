3d2fbf330213fc15b09dff7c9f448527668013f3
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-10-05 06:26:36 +0900

    base ok

--- src/components/section/archives/ArchivesSection.tsx
+++ src/components/section/archives/ArchivesSection.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 
 import { extraPageInfo, getExtraPageLink, getMainPageLink, mainPageInfo } from "@/models/archives";
 import { WithTitle } from "@/utils/hocs/WithTitle";
+import { WithWordBreak } from "@/utils/hocs/WithWordBreak";
 
 import { SectionContainer } from "@/components/shared/Container";
 
@@ -10,36 +11,40 @@ import { styles } from "./ArchivesSection.css";
 export const ArchivesSection: FC = () => (
     <SectionContainer id="archives" title="ARCHIVES">
         <div className={styles.root}>
-            <div>
-                <WithTitle title="EXTRA" size="lg">
-                    <nav>
-                        <ul>
-                            {extraPageInfo.map((page) => (
-                                <li key={page.id}>
-                                    <a
-                                        href={getExtraPageLink(page.id)}
-                                    >{`iiiExhibition Extra ${page.id} ${page.concept}`}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                </WithTitle>
-            </div>
-            <div>
-                <WithTitle title="III Exhibition" size="lg">
-                    <nav>
-                        <ul>
-                            {mainPageInfo.map((page) => (
-                                <li key={page.id}>
-                                    <a
-                                        href={getMainPageLink(page.id)}
-                                    >{`iiiExhibition ${page.id + 1998} ${page.concept}`}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                </WithTitle>
-            </div>
+            <WithTitle title="EXTRA" size="lg">
+                <nav>
+                    <ul>
+                        {extraPageInfo.map((page) => (
+                            <li key={page.id}>
+                                <a href={getExtraPageLink(page.id)}>
+                                    <WithWordBreak align="left">
+                                        {`iiiExhibition Extra ${page.id}`}
+                                        <wbr />
+                                        {page.concept}
+                                    </WithWordBreak>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </WithTitle>
+            <WithTitle title="III Exhibition" size="lg">
+                <nav>
+                    <ul>
+                        {mainPageInfo.map((page) => (
+                            <li key={page.id}>
+                                <a href={getMainPageLink(page.id)}>
+                                    <WithWordBreak align="left">
+                                        {`iiiExhibition ${page.id + 1998}`}
+                                        <wbr />
+                                        {page.concept}
+                                    </WithWordBreak>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </WithTitle>
         </div>
     </SectionContainer>
 );
