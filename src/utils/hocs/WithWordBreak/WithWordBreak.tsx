e78af9798b0fbb3f896cb392e185c242a0399e3a
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-10-04 19:46:15 +0900

    base ok

--- src/utils/hocs/WithWordBreak/WithWordBreak.tsx
+++ src/utils/hocs/WithWordBreak/WithWordBreak.tsx
@@ -1,13 +1,18 @@
 import clsx from "clsx";
-import { FC } from "react";
+import { ElementType, FC } from "react";
 
 import { styles } from "./WithWordBreak.css";
 
 type Props = {
     children: React.ReactNode;
+    align?: "center" | "left";
     className?: string;
+    as?: ElementType;
 };
 
-export const WithWordBreak: FC<Props> = ({ children, className }) => (
-    <p className={clsx(styles.root, className)}>{children}</p>
-);
+export const WithWordBreak: FC<Props> = ({
+    children,
+    align = "center",
+    className,
+    as: Component = "p",
+}) => <Component className={clsx(styles.root({ align }), className)}>{children}</Component>;
