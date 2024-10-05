fe70294d5c9ccbeb0af5c94d56cbf5512445d853
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-10-06 00:38:24 +0900

    update model

--- src/models/access.ts
+++ src/models/access.ts
@@ -1,5 +1,5 @@
 export const trainInfo = [
-    { line: "千代田線 根津駅", time: 8 },
-    { line: "南北線 東大前駅", time: 5 },
-    { line: "丸ノ内線 ・ 都営大江戸線 本郷三丁目駅", time: 8 },
-];
+    { line: "千代田線", station: "根津駅", time: 8 },
+    { line: "南北線", station: "東大前駅", time: 5 },
+    { line: "丸ノ内線 ・ 都営大江戸線", station: "本郷三丁目駅", time: 8 },
+] as const;
