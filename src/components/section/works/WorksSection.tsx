711e5b7a1e8c6ab36c57a7550f098381294137f2
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-10-05 16:52:33 +0900

    refactor ok

--- src/components/section/works/WorksSection.tsx
+++ src/components/section/works/WorksSection.tsx
@@ -11,20 +11,16 @@ import { WorksPickUp } from "@/components/ui/WorksPickUp";
 import { styles } from "./WorksSection.css";
 
 export const WorksSection: FC = () => (
-    <SectionContainer id="works" title="WORKS">
-        <div className={styles.root}>
-            <SubContainer label="ピックアップ" withBorder>
-                <WorksPickUp slides={workList} />
-            </SubContainer>
-            <SubContainer label="全作品">
-                <div className={styles.allWorks}>
-                    {placeList.map((place) => {
-                        const works = workList.filter((work) => work.place === place);
-                        return <WorksCarousel works={works} place={place} key={place} />;
-                    })}
-                </div>
-            </SubContainer>
-        </div>
+    <SectionContainer id="works" title="WORKS" className={styles.root}>
+        <SubContainer label="ピックアップ" withBorder>
+            <WorksPickUp slides={workList} />
+        </SubContainer>
+        <SubContainer label="全作品" className={styles.allWorks}>
+            {placeList.map((place) => {
+                const works = workList.filter((work) => work.place === place);
+                return <WorksCarousel works={works} place={place} key={place} />;
+            })}
+        </SubContainer>
     </SectionContainer>
 );
 
@@ -32,11 +28,17 @@ type SubContainerProps = {
     label: string;
     children: React.ReactNode;
     withBorder?: boolean;
+    className?: string;
 };
 
-export const SubContainer: FC<SubContainerProps> = ({ label, withBorder = false, children }) => (
+export const SubContainer: FC<SubContainerProps> = ({
+    label,
+    withBorder = false,
+    children,
+    className,
+}) => (
     <div>
         <h2 className={styles.subLabel({ withBorder })}>{label}</h2>
-        {children}
+        <div className={className}>{children}</div>
     </div>
 );
