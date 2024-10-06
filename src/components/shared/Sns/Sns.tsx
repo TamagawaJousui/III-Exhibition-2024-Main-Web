a29c9890c115717a2725a39dae8f94895dee7e39
Kaiba <yt.kaibaoke@gmail.com>
2024-10-07 02:40:46 +0900

    Snsリンクを追加

--- src/components/shared/Sns/Sns.tsx
+++ src/components/shared/Sns/Sns.tsx
@@ -15,14 +15,37 @@ type Props = {
 
 const ICON_SIZE = "1.5rem" as const;
 
-/**
- * TODO: リンクをユーザ用と制作展用でpropsで切り替える
- */
+// SNSプロフィールのURLを定義
+const SNS_URLS = {
+    instagram: "https://www.instagram.com/",
+    twitter: "https://twitter.com/",
+    facebook: "https://www.facebook.com/",
+};
+
+// Webでは動作しないので未使用
+// const X_URL = () => {
+//     var twitterAppUrl = "twitter://user?screen_name=self";
+//     var twitterWebUrl = "https://twitter.com/home";
+
+//     // まずアプリを開こうとする
+//     window.location.href = twitterAppUrl;
+
+//     // 少し遅延を入れてから、アプリが開かなかった場合にWebバージョンを開く
+//     setTimeout(function () {
+//         window.location.href = twitterWebUrl;
+//     }, 1000);
+// }
 
 export const Sns: FC<Props> = ({ size, gap = "sm", className }) => (
     <div className={clsx(styles.root({ gap }), className)}>
-        <InstagramLogo size={size ?? ICON_SIZE} />
-        <XLogo size={ICON_SIZE} />
-        <FacebookLogo size={ICON_SIZE} />
+        <a href={SNS_URLS.instagram} target="_blank" rel="noopener noreferrer">
+            <InstagramLogo size={size ?? ICON_SIZE} />
+        </a>
+        <a href={SNS_URLS.twitter} target="_blank" rel="noopener noreferrer">
+            <XLogo size={ICON_SIZE} />
+        </a>
+        <a href={SNS_URLS.facebook} target="_blank" rel="noopener noreferrer">
+            <FacebookLogo size={ICON_SIZE} />
+        </a>
     </div>
 );
