45dc544dd86c4ef39e1a0566f5f550faa3dd2f96
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-10-09 20:28:22 +0900

    fix ok

--- src/components/shared/Header/Header.css.ts
+++ src/components/shared/Header/Header.css.ts
@@ -29,7 +29,7 @@ export const styles = {
                 flexDirection: "column",
                 gap: vars.spacing.md,
                 fontFamily: "var(--font-playfair)",
-                fontSize: vars.fontSize.xl,
+                fontSize: vars.fontSize["2xl"],
             },
         },
     }),
@@ -40,6 +40,7 @@ export const styles = {
             [mediaUtils.mobile]: {
                 flexDirection: "column",
                 gap: vars.spacing.md,
+                width: "fit-content",
             },
         },
     }),
@@ -49,7 +50,7 @@ export const styles = {
         left: 0,
         width: "100vw",
         height: "100vh",
-        background: "rgba(182, 206, 228, 0.5)",
+        background: "rgba(182, 206, 228, 0.9)",
         zIndex: vars.zIndex.modal,
     }),
     desktopView: style({
