972d763fe0faa022a5006cc5a92d87ac479a8dd9
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-10-19 17:41:47 +0900

    move ConceptElement outside

--- src/components/section/concept/ConceptSection.tsx
+++ src/components/section/concept/ConceptSection.tsx
@@ -15,6 +15,19 @@ import { styles } from "./ConceptSection.css";
 // SSRを無効にしてreact-water-waveを動的にインポート
 const WaterWave = dynamic(() => import("react-water-wave"), { ssr: false });
 
+const ConceptElement = () => (
+    <div className={styles.container}>
+        <div className={styles.concept}>
+            {conceptData.ja.map((concept) => (
+                <WithWordBreak key={concept} as="h4" align="center">
+                    <BreakWord content={concept} optional />
+                </WithWordBreak>
+            ))}
+        </div>
+        <p className={styles.concept}>{conceptData.en}</p>
+    </div>
+);
+
 export const ConceptSection: FC = () => {
     const [isWebGlSupported, setIsWebGlSupported] = useState(false);
 
@@ -24,28 +37,15 @@ export const ConceptSection: FC = () => {
         }
     }, []);
 
-    const conceptElement = (
-        <div className={styles.container}>
-            <div className={styles.concept}>
-                {conceptData.ja.map((concept) => (
-                    <WithWordBreak key={concept} as="h4" align="center">
-                        <BreakWord content={concept} optional />
-                    </WithWordBreak>
-                ))}
-            </div>
-            <p className={styles.concept}>{conceptData.en}</p>
-        </div>
-    );
-
-    const waterWaveWrapper = (element: React.ReactNode) => (
-        <WaterWave className={styles.wrapper} dropRadius={100}>
-            {() => element}
-        </WaterWave>
-    );
-
     return (
         <SectionContainer id="concept" title="CONCEPT" className={styles.root}>
-            {isWebGlSupported ? waterWaveWrapper(conceptElement) : conceptElement}
+            {isWebGlSupported ? (
+                <WaterWave className={styles.wrapper} dropRadius={100}>
+                    {ConceptElement}
+                </WaterWave>
+            ) : (
+                <ConceptElement />
+            )}
         </SectionContainer>
     );
 };
