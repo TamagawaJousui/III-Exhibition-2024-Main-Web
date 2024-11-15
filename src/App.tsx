64bdb9cb738831ed0b1edd9b9ca3e33cc231964c
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-15 16:06:53 +0900

    add heroarea, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-export default function App() {
+import "./App.css";
+import HeroArea from "./component/HeroArea";
+import Concept from "./component/Concept";
+import Works from "./component/Works";
+import Access from "./component/Access";
+import Announce from "./component/Announce";
+import Members from "./component/Members";
+import Archives from "./component/Archives";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="grid w-max bg-gradient grid-flow-row md:grid-flow-col md:auto-cols-max ">
+      <HeroArea />
+      <Concept />
+      <Works />
+      <Access />
+      <Announce />
+      <Members />
+      <Archives />
+    </div>
+  );
 }
+
+export default App;
