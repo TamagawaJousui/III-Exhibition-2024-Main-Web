b4c02d3730627042a3b62ae51f3714fe58a5459d
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-17 12:43:14 +0900

    pickups

--- src/component/PickUps.tsx
+++ src/component/PickUps.tsx
@@ -3,15 +3,16 @@ import Fade from "embla-carousel-fade";
 import Autoplay from "embla-carousel-autoplay";
 import { workList as slides } from "@/models/works";
 import { IoLocationSharp } from "react-icons/io5";
+// import PickupButton from "@/assets/works/PickupButton.svg?react";
 
 export default function PickUps() {
   const [emblaRef] = useEmblaCarousel({ loop: true }, [
     Fade(),
-    Autoplay({ playOnInit: true, delay: 1000 }),
+    Autoplay({ playOnInit: true, delay: 1500 }),
   ]);
   return (
     <>
-      <div className="text-2xl text-primary font-serif font-extrabold border-b-[1px] border-line">
+      <div className="border-b border-line font-serif text-2xl font-extrabold text-primary">
         ピックアップ
       </div>
       <div className="h-2"></div>
@@ -19,43 +20,48 @@ export default function PickUps() {
         <div className="flex">
           {slides.map((slide) => (
             <div
-              className="flex-[0_0_100%] min-w-0"
+              className="min-w-0 flex-[0_0_100%]"
               key={`${slide.title}-${slide.place}`}
             >
-              <div className="bg-pickup-background rounded-3xl">
-                <div className="relative w-full aspect-square bg-primary rounded-3xl">
+              <div className="aspect-[1.618] max-w-screen-sm rounded-3xl bg-pickup-background">
+                <div className="relative aspect-square w-full rounded-3xl ">
                   <img
                     src={slide.imagePath}
                     alt={`works image of ${slide.title}`}
-                    className="w-full aspect-square rounded-3xl"
+                    className="aspect-square w-full rounded-3xl"
                   />
                   <img
                     src="/works/random_texture.webp"
                     alt="random texture"
-                    className="absolute top-0 left-0 w-full aspect-square rounded-3xl"
+                    className="absolute left-0 top-0 aspect-square w-full rounded-3xl"
                   />
                 </div>
-                <div className="px-4 pt-4 text-pickup-text h-[200px]">
-                  <div className="pb-2 font-works-title italic text-2xl border-b-[1px] border-pickup-text w-fit">
-                    {slide.title}
+                <div className="flex h-full flex-col justify-between px-4 pt-4 text-pickup-text">
+                  <div>
+                    <div className="w-fit break-keep border-b border-pickup-text pb-2 font-works-title text-2xl italic">
+                      {slide.title}
+                    </div>
+                    <p className="pt-2 font-gothic text-base">
+                      {slide.member.map((member, index) => (
+                        <span key={member} className="break-keep">
+                          {index > 0 && " /\u200B "}
+                          {member}
+                        </span>
+                      ))}
+                    </p>
                   </div>
-                  <p className="pt-2 font-gothic text-base max-w-[70%]">
-                    {slide.member.map((member, index) => (
-                      <span key={member} className="break-keep">
-                        {index > 0 && " /\u200B "}
-                        {member}
+                  <div>
+                    <p className="flex items-center pt-4">
+                      <IoLocationSharp className="text-white" />
+                      <span className="pl-2 font-gothic text-sm">
+                        {slide.place}
                       </span>
-                    ))}
-                  </p>
-                  <p className="pt-4 pb-2 flex items-center">
-                    <IoLocationSharp className="text-white" />
-                    <span className="pl-2 font-gothic text-sm text-[#5e5e5e]">
-                      {slide.place}
-                    </span>
-                  </p>
-                  <div className="flex justify-end pb-4 pr-4">
-                    <div className="text-base font-serif text-center w-24 bg-white rounded-full">
-                      詳細を見る
+                    </p>
+                    <div className="flex justify-end py-2">
+                      <div className="h-6 w-24 rounded-full bg-white text-center font-serif text-base text-white [-webkit-text-stroke:_0.4px_black;] [paint-order:stroke_fill]">
+                        {/* <PickupButton /> */}
+                        詳細を見る
+                      </div>
                     </div>
                   </div>
                 </div>
