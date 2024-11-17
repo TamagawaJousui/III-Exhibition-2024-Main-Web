623300b631ad9620b0fef3fd88813fa8af7c71b1
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-18 03:13:01 +0900

    add md:pt for works

--- src/component/Works.tsx
+++ src/component/Works.tsx
@@ -4,7 +4,7 @@ export default function Works() {
   return (
     <section className="section-container">
       <div className="section-title">WORKS</div>
-      <div className="section-content pb-0 pt-6">
+      <div className="section-content pb-0 pt-6 md:pt-16">
         <PickUps />
       </div>
     </section>
