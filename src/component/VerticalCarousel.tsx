94d8956d03fd97849908d9738f30dac7f4c22719
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-18 23:28:44 +0900

    basic layout

--- src/component/VerticalCarousel.tsx
+++ src/component/VerticalCarousel.tsx
@@ -1,44 +1,56 @@
 import useEmblaCarousel from 'embla-carousel-react'
+import { useEffect } from 'react'
 
 interface VerticalCarouselProps {
     index: number
 }
+const getBgColor = (index: number) => {
+    switch (index) {
+        case 0:
+            return 'bg-works-carousel-forum'
+        case 1:
+            return 'bg-works-carousel-exhibition'
+        case 2:
+            return 'bg-works-carousel-sky'
+        case 3:
+            return 'bg-works-carousel-open'
+        default:
+            return 'bg-works-carousel-forum'
+    }
+}
+
+const getPlace = (index: number) => {
+    switch (index) {
+        case 0:
+            return '[工学部2号館2F フォーラム]'
+        case 1:
+            return '[工学部2号館2F 展示室]'
+        case 2:
+            return '[工学部2号館9F 92B教室]'
+        case 3:
+            return '[情報学環オープンスタジオ]'
+        default:
+            return '[工学部2号館2F フォーラム]'
+    }
+}
 
 export default function VerticalCarousel({ index }: VerticalCarouselProps) {
-    const getBgColor = (index: number) => {
-        switch (index) {
-            case 0:
-                return 'bg-works-carousel-forum'
-            case 1:
-                return 'bg-works-carousel-exhibition'
-            case 2:
-                return 'bg-works-carousel-sky'
-            case 3:
-                return 'bg-works-carousel-open'
-            default:
-                return 'bg-works-carousel-forum'
-        }
-    }
-
-    const getPlace = (index: number) => {
-        switch (index) {
-            case 0:
-                return '[工学部2号館2F フォーラム]'
-            case 1:
-                return '[工学部2号館2F 展示室]'
-            case 2:
-                return '[工学部2号館9F 92B教室]'
-            case 3:
-                return '[情報学環オープンスタジオ]'
-            default:
-                return '[工学部2号館2F フォーラム]'
-        }
-    }
-
     const place = getPlace(index)
     const bgColor = getBgColor(index)
 
-    const [emblaRef] = useEmblaCarousel({ axis: 'y' })
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: true,
+        axis: 'y',
+        dragFree: true,
+        containScroll: false,
+        watchSlides: false,
+    })
+
+    useEffect(() => {
+        if (!emblaApi) {
+            return
+        }
+    })
 
     return (
         <div className="size-full border border-divider-line">
@@ -59,9 +71,9 @@ export default function VerticalCarousel({ index }: VerticalCarouselProps) {
                     {[0, 1, 2, 3, 5, 6, 7, 8].map((number) => (
                         <div
                             key={number}
-                            className="flex min-w-0 flex-[0_0_30%]"
+                            className="flex min-h-0 flex-[0_0_8rem]"
                         >
-                            <div className="size-36 bg-primary/60">
+                            <div className="size-28 bg-primary/60">
                                 {number}
                             </div>
                         </div>
