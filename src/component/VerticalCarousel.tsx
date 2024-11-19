0f00fa499ad681de5026b286da139b921f69775d
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-20 01:42:26 +0900

    show pics

--- src/component/VerticalCarousel.tsx
+++ src/component/VerticalCarousel.tsx
@@ -1,46 +1,21 @@
 import useEmblaCarousel from 'embla-carousel-react'
 import { useCallback, useEffect, useRef } from 'react'
 import { EmblaCarouselType } from 'embla-carousel'
+import { placeList, placeColorPalette } from '@/models/place'
+import { workList } from '@/models/works'
 
 // import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
 
 interface VerticalCarouselProps {
+    // should be 0-3
     index: number
-}
-const getBgColor = (index: number) => {
-    switch (index) {
-        case 0:
-            return 'bg-works-carousel-forum'
-        case 1:
-            return 'bg-works-carousel-exhibition'
-        case 2:
-            return 'bg-works-carousel-sky'
-        case 3:
-            return 'bg-works-carousel-open'
-        default:
-            return 'bg-works-carousel-forum'
-    }
-}
-
-const getPlace = (index: number) => {
-    switch (index) {
-        case 0:
-            return '[工学部2号館2F フォーラム]'
-        case 1:
-            return '[工学部2号館2F 展示室]'
-        case 2:
-            return '[工学部2号館9F Ｓｋｙ]'
-        case 3:
-            return '[情報学環オープンスタジオ]'
-        default:
-            return '[工学部2号館2F フォーラム]'
-    }
 }
 
 export default function VerticalCarousel({ index }: VerticalCarouselProps) {
-    const place = getPlace(index)
-    const bgColor = getBgColor(index)
+    const place = placeList[index]
+    const bgColor = `bg-${placeColorPalette[place]}`
     const progressBarRef = useRef<HTMLDivElement>(null)
+    const slides = workList.filter((work) => work.place === place)
 
     const [emblaRef, emblaApi] = useEmblaCarousel(
         {
@@ -139,85 +114,97 @@ export default function VerticalCarousel({ index }: VerticalCarouselProps) {
             <div
                 className={`h-12 w-full ${bgColor} flex items-center justify-center`}
             >
-                <div className="text-works-carousel-title font-serif text-xl font-medium">
-                    {place}
+                <div
+                    className={`text-works-carousel-title whitespace-pre-wrap text-center font-serif font-medium ${
+                        place === placeList[2]
+                            ? 'text-base leading-4'
+                            : 'text-xl'
+                    } `}
+                >
+                    {place.replace('・', '\n')}
                 </div>
             </div>
             {/* embla__viewport */}
-            <div
-                className="perspective-400 perspective-origin-center flex h-[calc(100%-3rem)] w-full items-center justify-center overflow-hidden"
-                ref={emblaRef}
-            >
-                {/* embla__container */}
-                <div className="transform-style-3d flex h-1/3 w-full touch-pan-x touch-pinch-zoom flex-col items-center">
-                    <PlaceHolder />
-                    <PlaceHolder />
-                    {[0, 1, 2, 3, 4, 5, 6, 7].map((number) => (
-                        <div
-                            key={number}
-                            className="my-1 flex min-h-0 w-full flex-[0_0_100%] items-center justify-center"
-                        >
-                            <div className="relative aspect-square h-full rounded-3xl border-2 border-green-500 bg-primary/60">
-                                {number + 1}
-                                <div className="absolute left-0 top-1/2 h-[2px] w-full bg-red-500"></div>
-                                <div className="absolute left-1/2 top-0 h-full w-[2px] bg-red-500"></div>
-                            </div>
-                        </div>
-                    ))}
-                    <PlaceHolder />
-                    <PlaceHolder />
-                </div>
-                {/* progress bar */}
-                <div className="absolute right-4 flex h-full flex-col justify-center">
-                    <div className="flex h-2/5 flex-col items-center justify-between gap-4">
-                        {/* up arrow */}
-                        <div className="text-works-carousel-progress h-5 w-5">
-                            <svg viewBox="0 0 14 11" fill="none">
-                                <path
-                                    d="M10.8672 7.13281L6.67264 2.4005L2.47809 7.13281"
-                                    stroke="currentColor"
-                                    strokeWidth="2.1"
-                                    strokeLinecap="round"
-                                    className="drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.25)]"
-                                />
-                            </svg>
-                        </div>
-
-                        {/* dots container */}
-                        <div className="relative flex flex-1 flex-col justify-between gap-1">
-                            {/* moving line */}
+            <div className="h-[calc(100%-6rem)] w-full">
+                <div
+                    className="perspective-400 perspective-origin-center flex h-full w-full items-center justify-center overflow-hidden"
+                    ref={emblaRef}
+                >
+                    {/* embla__container */}
+                    <div className="transform-style-3d flex h-1/3 w-full touch-pan-x touch-pinch-zoom flex-col items-center">
+                        <PlaceHolder />
+                        <PlaceHolder />
+                        {slides.map((slide) => (
                             <div
-                                ref={progressBarRef}
-                                className="bg-works-carousel-progress-line absolute left-1/2 z-10 h-[25%] w-1 -translate-x-1/2 rounded-full"
-                                style={{
-                                    top: '0%',
-                                    transition: 'top 0.3s ease',
-                                }}
-                            />
+                                key={slide.title}
+                                className="my-1 flex min-h-0 w-full flex-[0_0_100%] items-center justify-center"
+                            >
+                                <div className="relative aspect-square h-full rounded-3xl">
+                                    <img
+                                        src={slide.imagePath}
+                                        alt={slide.title}
+                                        className='rounded-3xl'
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                        <PlaceHolder />
+                        <PlaceHolder />
+                    </div>
+                    {/* progress bar */}
+                    <div className="absolute right-4 flex h-full flex-col justify-center">
+                        <div className="flex h-2/5 flex-col items-center justify-between gap-4">
+                            {/* up arrow */}
+                            <div className="text-works-carousel-progress h-5 w-5">
+                                <svg viewBox="0 0 14 11" fill="none">
+                                    <path
+                                        d="M10.8672 7.13281L6.67264 2.4005L2.47809 7.13281"
+                                        stroke="currentColor"
+                                        strokeWidth="2.1"
+                                        strokeLinecap="round"
+                                        className="drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.25)]"
+                                    />
+                                </svg>
+                            </div>
 
-                            {[...Array(16)].map((_, i) => (
+                            {/* dots container */}
+                            <div className="relative flex flex-1 flex-col justify-between gap-1">
+                                {/* moving line */}
                                 <div
-                                    key={i}
-                                    className="bg-works-carousel-progress h-1 w-1 rounded-full drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.25)]"
-                                ></div>
-                            ))}
-                        </div>
-
-                        {/* down arrow */}
-                        <div className="text-works-carousel-progress flex h-5 w-5 flex-col justify-end">
-                            <svg viewBox="0 0 14 11" fill="none">
-                                <path
-                                    d="M2.47809 2.4005L6.67264 7.13281L10.8672 2.4005"
-                                    stroke="currentColor"
-                                    strokeWidth="2.1"
-                                    strokeLinecap="round"
-                                    className="drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.25)]"
+                                    ref={progressBarRef}
+                                    className="bg-works-carousel-progress-line absolute left-1/2 z-10 h-[25%] w-1 -translate-x-1/2 rounded-full"
+                                    style={{
+                                        top: '0%',
+                                        transition: 'top 0.3s ease',
+                                    }}
                                 />
-                            </svg>
+
+                                {[...Array(16)].map((_, i) => (
+                                    <div
+                                        key={i}
+                                        className="bg-works-carousel-progress h-1 w-1 rounded-full drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.25)]"
+                                    ></div>
+                                ))}
+                            </div>
+
+                            {/* down arrow */}
+                            <div className="text-works-carousel-progress flex h-5 w-5 flex-col justify-end">
+                                <svg viewBox="0 0 14 11" fill="none">
+                                    <path
+                                        d="M2.47809 2.4005L6.67264 7.13281L10.8672 2.4005"
+                                        stroke="currentColor"
+                                        strokeWidth="2.1"
+                                        strokeLinecap="round"
+                                        className="drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.25)]"
+                                    />
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {/* footer */}
+            <div className="h-12 w-full bg-black"></div>
         </div>
     )
 }
--- src/models/place.ts
+++ src/models/place.ts
@@ -1,15 +1,15 @@
 export const placeList = [
-    "工学部2号館フォーラム",
-    "工学部2号館展示室",
-    "工学部2号館9階・Sky Presentation Room",
-    "情報学環オープンスタジオ",
-] as const;
+    '工学部2号館フォーラム',
+    '工学部2号館展示室',
+    '工学部2号館9階・Sky Presentation Room',
+    '情報学環オープンスタジオ',
+] as const
 
-export type Place = (typeof placeList)[number];
+export type Place = (typeof placeList)[number]
 
 export const placeColorPalette = {
-    工学部2号館フォーラム: "#5F5A62",
-    "工学部2号館9階・Sky Presentation Room": "#7D7E73",
-    工学部2号館展示室: "#7E7873",
-    情報学環オープンスタジオ: "#7E7373",
-} as const satisfies Record<Place, string>;
+    工学部2号館フォーラム: 'works-carousel-forum',
+    '工学部2号館9階・Sky Presentation Room': 'works-carousel-sky',
+    工学部2号館展示室: 'works-carousel-exhibition',
+    情報学環オープンスタジオ: 'works-carousel-open',
+} as const satisfies Record<Place, string>
