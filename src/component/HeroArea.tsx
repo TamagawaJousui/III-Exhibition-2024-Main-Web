db226685b49fe961c285052b29b7da174b22ce5e
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-12-11 00:13:46 +0900

    disable no-constant-condition for file

--- src/component/HeroArea.tsx
+++ src/component/HeroArea.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-constant-condition */
 import StaticTitle from "@/assets/heroarea/Title.svg?react";
 import StaticTitleEnglish from "@/assets/heroarea/TitleEnglish.svg?react";
 import StaticDateAndVenue from "@/assets/heroarea/DateAndVenue.svg?react";
@@ -31,7 +32,6 @@ export default function HeroArea() {
 
   return (
     <section className="section-container h-svh min-h-svh w-screen overflow-hidden">
-      // eslint-disable-next-line no-constant-condition
       {showParticles && false ? (
         <>
           <Title />
