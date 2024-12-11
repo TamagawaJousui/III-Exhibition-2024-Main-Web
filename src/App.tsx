8439d9fecd6948fa72d4aa7c15f14af3501cfcda
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-12-11 23:28:17 +0900

    support vertical scroll but break horizontal scroll snap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import { lazy } from "react";
+import { lazy, useEffect } from "react";
 import Header from "./component/Header";
+import { breakpoint } from "./utils/BreakPoint";
 const HeroArea = lazy(() => import("./component/HeroArea"));
 const Concept = lazy(() => import("./component/Concept"));
 const Works = lazy(() => import("./component/Works"));
@@ -10,6 +11,46 @@ const Members = lazy(() => import("./component/Members"));
 const Archives = lazy(() => import("./component/Archives"));
 
 function App() {
+  const mediaQuery = window.matchMedia(`(min-width: ${breakpoint.md}px)`);
+
+  useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      e.preventDefault();
+      const scrollSpeed = 1;
+
+      const magnitude = Math.sqrt(e.deltaX * e.deltaX + e.deltaY * e.deltaY);
+      const direction = e.deltaY > -e.deltaX ? 1 : -1;
+
+      window.scrollBy({
+        left: magnitude * direction * scrollSpeed,
+        behavior: "auto",
+      });
+    };
+
+    const handleMediaChange = (e: MediaQueryListEvent | MediaQueryList) => {
+      if (e.matches) {
+        window.addEventListener("wheel", handleWheel, {
+          passive: false,
+          capture: true,
+        });
+      } else {
+        window.removeEventListener("wheel", handleWheel, {
+          capture: true,
+        });
+      }
+    };
+
+    handleMediaChange(mediaQuery);
+    mediaQuery.addEventListener("change", handleMediaChange, { passive: true });
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaChange);
+      window.removeEventListener("wheel", handleWheel, {
+        capture: true,
+      });
+    };
+  }, [mediaQuery]);
+
   return (
     <>
       <Header />
