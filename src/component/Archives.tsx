b14a47430347f811bf0e6ff1617010c422c7bc5f
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-12-11 21:17:17 +0900

    change archives

--- src/component/Archives.tsx
+++ src/component/Archives.tsx
@@ -11,10 +11,8 @@ export default function Archives() {
       <div className="section-title">ARCHIVES</div>
       <div className="section-content grid gap-16 md:grid-flow-row md:grid-cols-[minmax(0,_500px)_minmax(0,_550px)] md:pt-16 2xl:grid-cols-[minmax(0,_768px)_minmax(0,_768px)]">
         <div className="max-w-screen-sm 2xl:max-w-none">
-          <div className="second-title text-2xl md:text-3xl 2xl:text-4xl">
-            EXTRA
-          </div>
-          <nav className="pt-4 font-gothic text-base text-primary md:text-lg 2xl:text-2xl">
+          <div className="second-title text-2xl">EXTRA</div>
+          <nav className="pt-4 font-gothic text-base text-primary md:text-lg">
             <ul className="flex flex-col gap-4 md:gap-6">
               {extraPageInfo.map((page) => (
                 <li key={page.id}>
@@ -32,10 +30,8 @@ export default function Archives() {
           </nav>
         </div>
         <div className="max-w-screen-sm md:pt-0 2xl:max-w-none">
-          <div className="second-title text-2xl md:text-3xl 2xl:text-4xl">
-            III Exhibition
-          </div>
-          <nav className="pt-4 font-gothic text-base text-primary md:text-lg 2xl:text-2xl">
+          <div className="second-title text-2xl">III Exhibition</div>
+          <nav className="pt-4 font-gothic text-base text-primary md:text-lg">
             <ul className="flex flex-col gap-4 md:gap-6">
               {mainPageInfo.map((page) => (
                 <li key={page.id}>
