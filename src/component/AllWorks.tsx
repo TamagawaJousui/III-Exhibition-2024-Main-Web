c3e185d5c2aa4f5f7428f6ebffb8202639abd1d3
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-12-11 18:28:55 +0900

    dom structure refine

--- src/component/AllWorks.tsx
+++ src/component/AllWorks.tsx
@@ -5,16 +5,14 @@ import { placeList } from "@/models/place";
 export default function AllWorks() {
   return (
     <div className="md: mt-24 snap-start scroll-m-12 md:ml-8 md:mt-0">
-      <div className="second-title border-0">全作品</div>
-      <div className="flex flex-col gap-8 pt-4 md:max-w-[1800px] md:flex-row md:overflow-hidden md:pt-0">
+      <div className="second-title w-fit border-0">全作品</div>
+      <div className="flex flex-col gap-8 pt-4 md:flex-row md:pt-0">
         {placeList.map((place, index) => (
           <div
             key={place}
-            className="ml-1 flex min-w-0 flex-[0_0_80%] snap-start scroll-m-12 md:flex-[0_0_400px]"
+            className="h-[80vh] w-full snap-start scroll-m-12 md:w-96"
           >
-            <div className="h-[80vh] w-full">
-              <VerticalCarousel index={index} />
-            </div>
+            <VerticalCarousel index={index} />
           </div>
         ))}
       </div>
