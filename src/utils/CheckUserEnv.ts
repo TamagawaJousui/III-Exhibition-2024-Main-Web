15e18c30c3166bfd7ee9bad1ca8224e0d026ce60
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-12-14 22:18:53 +0900

    check windows for ssg

--- src/utils/CheckUserEnv.ts
+++ src/utils/CheckUserEnv.ts
@@ -2,35 +2,45 @@ import * as THREE from "three";
 import { UAParser } from "ua-parser-js";
 
 export const isMobileDevice = () => {
-    const ua = navigator.userAgent;
-    const device = new UAParser(ua).getDevice();
-    return device.type === "mobile";
+  const ua = navigator.userAgent;
+  const device = new UAParser(ua).getDevice();
+  return device.type === "mobile";
 };
 
 const isWebGLSupported = (() => {
-    try {
-        const canvas = document.createElement("canvas");
-        return !!window.WebGLRenderingContext && !!canvas.getContext("webgl");
-    } catch (e) {
-        console.error(e);
-        return false;
-    }
+  if (typeof window === "undefined") {
+    return false;
+  }
+
+  try {
+    const canvas = document.createElement("canvas");
+    return !!window.WebGLRenderingContext && !!canvas.getContext("webgl");
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 })();
 
 const isDataTextureSupported = (() => {
-    try {
-        const texture = new THREE.DataTexture(new Uint8Array(4), 1, 1, THREE.RGBAFormat);
-        return texture instanceof THREE.DataTexture;
-    } catch (e) {
-        console.error(e);
-        return false;
-    }
+  try {
+    const texture = new THREE.DataTexture(
+      new Uint8Array(4),
+      1,
+      1,
+      THREE.RGBAFormat
+    );
+    return texture instanceof THREE.DataTexture;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 })();
 
-export const isWebglCompatible = () => isWebGLSupported && isDataTextureSupported;
+export const isWebglCompatible = () =>
+  isWebGLSupported && isDataTextureSupported;
 
 export const isWebGlCapable = () => {
-    const isMobile = isMobileDevice();
-    const isWebGlSupported = isWebglCompatible();
-    return !isMobile && isWebGlSupported;
+  const isMobile = isMobileDevice();
+  const isWebGlSupported = isWebglCompatible();
+  return !isMobile && isWebGlSupported;
 };
